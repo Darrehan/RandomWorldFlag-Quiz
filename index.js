fa086f04f4f5c1c534480b05853eef47f8f33325
Darrehan <darrehanrasool@gmail.com>
2024-01-22 04:14:28 +0530

    Dar Rehan Rasool
    sycomusiclover

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/",   (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
